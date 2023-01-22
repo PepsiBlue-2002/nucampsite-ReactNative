d3463e6086c81d80924f35a08084394a08fbb9ce
D <technicallyanemail22@gmail.com>
2023-01-22 18:03:34 -0500

    week 3

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -8,19 +8,14 @@ const RenderCampsite = (props) => {
         return (
             <Card containerStyle={styles.cardContainer}>
                 <Card.Image source={{ uri: baseURL + campsite.image }}>
-                    <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text
-                            style={{
-                                color: 'white',
-                                textAlign: 'center',
-                                fontSize: 20
-                            }}
-                        >
+                    <View style={styles.cardRow}>
+                        <Text style={styles.cardText}>
                             {campsite.name}
                         </Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
+                <View style={styles.cardRow}>
                 <Icon
                     name={props.isFavorite ? 'heart' : 'heart-o'}
                     type='font-awesome'
@@ -33,6 +28,15 @@ const RenderCampsite = (props) => {
                             : props.markFavorite()
                     }
                 />
+                <Icon
+                    name='pencil'
+                    type='font-awesome'
+                    color='#5637DD'
+                    raised
+                    reverse
+                    onPress={() => props.onShowModal()}
+                />
+                </View>
             </Card>
         );
     }
@@ -44,6 +48,21 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
+    },
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
     }
 });
 
--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -2,23 +2,37 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseURL } from '../../shared/baseURL';
 
 export const fetchComments = createAsyncThunk(
-    'comments/fetchComments',
+    "comments/fetchComments",
     async () => {
-        const response = await fetch(baseURL + 'comments');
+        const response = await fetch(baseURL + "comments");
         if (!response.ok) {
-            return Promise.reject(
-                'Unable to fetch, status: ' + response.status
-            );
+            return Promise.reject("Unable to fetch, status: " + response.status);
         }
         const data = await response.json();
         return data;
     }
 );
 
+export const postComment = createAsyncThunk(
+    "comments/postComment",
+    async (payload, { dispatch, getState }) => {
+        setTimeout(() => {
+            const { comments } = getState();
+            payload.date = new Date().toISOString();
+            payload.id = comments.commentsArray.length;
+            dispatch(addComment(payload));
+        }, 2000);
+    }
+);
+
 const commentsSlice = createSlice({
-    name: 'comments',
+    name: "comments",
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload);
+        },
+    },
     extraReducers: {
         [fetchComments.pending]: (state) => {
             state.isLoading = true;
@@ -30,11 +44,10 @@ const commentsSlice = createSlice({
         },
         [fetchComments.rejected]: (state, action) => {
             state.isLoading = false;
-            state.errMess = action.error
-                ? action.error.message
-                : 'Fetch failed';
-        }
-    }
+            state.errMess = action.error ? action.error.message : "Fetch failed";
+        },
+    },
 });
 
+export const { addComment } = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
