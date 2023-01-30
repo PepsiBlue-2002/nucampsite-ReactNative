a2a19de67e997cd2734b27e4729f644b64aa8af3
D <technicallyanemail22@gmail.com>
2023-01-30 07:41:14 -0500

    added email, toast, notifications, sharing, secure store, network info

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 import { Text, View, ScrollView, StyleSheet, 
-    Switch, Button } from 'react-native';
+    Switch, Button, Alert } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from "react-native-animatable";
-import { Alert } from "react-native";
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
     const [hikeIn, setHikeIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
-    const [showModal, setShowModal] = useState(false);
 
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -20,10 +19,37 @@ const ReservationScreen = () => {
     };
 
     const handleReservation = () => {
+        const message = `Number of Campers: ${campers}
+            \nHike-In? ${hikeIn}
+            \nDate: ${date.toLocaleDateString('en-US')}`;
+
+        Alert.alert(
+            'Begin Search?',
+            message,
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        resetForm();
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
+                }
+            ],
+            { cancelable: false }
+        );
         console.log('campers:', campers);
         console.log('hikeIn:', hikeIn);
         console.log('date:', date);
-        setShowModal(!showModal);
     };
 
     const resetForm = () => {
@@ -33,9 +59,37 @@ const ReservationScreen = () => {
         setShowCalendar(false);
     };
 
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
+
     return (
         <ScrollView>
-            <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
                     <Picker
@@ -80,29 +134,10 @@ const ReservationScreen = () => {
                 )}
                 <View style={styles.formRow}>
                     <Button
-                        onPress={() =>
-                        Alert.alert(
-                            "Begin Search?",
-                            `Number of campers: ${campers}
-                                \nHike-in: ${hikeIn}
-                                \nDate: ${date.toLocaleDateString("en-US")}`,
-                            [
-                            {
-                                text: "Cancel",
-                                onPress: () => resetForm(),
-                                style: "cancel",
-                            },
-                            {
-                                text: "Ok",
-                                onPress: () => resetForm(),
-                            },
-                            ],
-                            { cancelable: false }
-                        )
-                        }
-                        title="Search Availability"
-                        color="#5637DD"
-                        accessibilityLabel="Tap me to search for available campsites to reserve"
+                        onPress={() => handleReservation()}
+                        title='Search Availability'
+                        color='#5637DD'
+                        accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
             </Animatable.View>
@@ -126,5 +161,6 @@ const styles = StyleSheet.create({
         flex: 1
     }
 });
+
 
 export default ReservationScreen;
